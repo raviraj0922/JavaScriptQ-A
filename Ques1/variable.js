0428e035cc63155de5ba90256483772428977ef8
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2023-12-13 21:35:51 +0530

    variable.js file for ques1

---
+++ Ques1/variable.js
@@ -0,0 +1,13 @@
+// variable.js
+
+// Declare variables with different data types
+let stringVariable = "Hello, World!";
+let booleanVariable = true;
+let undefinedVariable;
+let nullVariable = null;
+
+// Display values and their data types
+console.log("String Variable:", stringVariable, typeof stringVariable);
+console.log("Boolean Variable:", booleanVariable, typeof booleanVariable);
+console.log("Undefined Variable:", undefinedVariable, typeof undefinedVariable);
+console.log("Null Variable:", nullVariable, typeof nullVariable);
