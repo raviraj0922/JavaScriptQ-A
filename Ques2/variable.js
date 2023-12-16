504aa89ada6c47b460672ef595c1228e17a7b862
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2023-12-16 13:16:41 +0530

    Uploaded the variable.js

---
+++ Ques2/variable.js
@@ -0,0 +1,9 @@
+// Declare variables
+const firstName = "Ravi";
+const lastName = "Raj";
+const maritalStatus = "Married";
+const country = "India";
+const age = 32;
+
+// Display using interpolation method
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old and ${maritalStatus}. I live in ${country}.`);
