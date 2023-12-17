b3a30a2fa02dd5ba029a50325d5b46cd09f60b93
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2023-12-17 20:13:24 +0530

    Variable.js file uploaded

---
+++ Ques3/variable.js
@@ -0,0 +1,9 @@
+// Declare a variable and assign a string value
+const message = "hello world!";
+
+// Convert the string to uppercase using toUpperCase() method
+const uppercaseMessage = message.toUpperCase();
+
+// Display the original and uppercase text
+console.log("Original message:", message);
+console.log("Uppercase message:", uppercaseMessage);
