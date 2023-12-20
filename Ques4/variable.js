c53627783202a1204775e80dbee2daf7c4cedbae
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2023-12-20 21:37:06 +0530

    Create variable.js

---
+++ Ques4/variable.js
@@ -0,0 +1,8 @@
+// Declare a variable and assign a string value
+const text = "This script written for a game.";
+
+// Check if the string contains the word "Script" using includes() method
+const containsScript = text.includes("Script");
+
+// Display the result
+console.log(`The text contains the word 'Script': ${containsScript}`);
