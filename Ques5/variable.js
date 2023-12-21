6e44387e9b24381b446799869ba343e7c0d64b0d
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2023-12-21 22:59:48 +0530

    Create variable.js

---
+++ Ques5/variable.js
@@ -0,0 +1,9 @@
+// Declare a variable and assign a string value
+const text = "I love to play games online.";
+
+// Split the string into an array using split() method
+const words = text.split();
+// const words = text.split(",");
+
+// Display the splitted words
+console.log("Splitted words:", words);
