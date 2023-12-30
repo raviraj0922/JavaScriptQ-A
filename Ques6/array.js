fdb0cb2e62a02bc2dac681ed33a72604a0107af3
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2023-12-30 15:55:53 +0530

    Create array.js

---
+++ Ques6/array.js
@@ -0,0 +1,7 @@
+const companiesString = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+
+// Split the string at the comma and convert it to an array
+const companiesArray = companiesString.split(", ");
+
+// Print the array
+console.log(companiesArray);
